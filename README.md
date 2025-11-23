5e74de74f2908226a836796d4ac5447871b5d7ff
Pavel <voroshilov.pavel@gmail.com>
2025-11-23 18:30:10 +0300

    feat:final

--- README.md
+++ README.md
@@ -1,4 +1,124 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/pavel-pj/go-project-244/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/pavel-pj/go-project-244/actions)
 
-### 14/11/2011.
+
+# GOlang Console utilite "GenDiff" 
+
+### Compares two files (json,yaml) and shows differencies in 3 types formats.
+
+
+[![asciicast](https://asciinema.org/a/757726.svg)](https://asciinema.org/a/757726)
+
+#### file01.json:
+```
+{
+"common": {
+  "setting1": "Value 1",
+  "setting2": 200,
+  "setting3": true,
+  "setting6": {
+    "key": "value",
+    "doge": {
+      "wow": ""
+    }
+  }
+},
+"group1": {
+  "baz": "bas",
+  "foo": "bar",
+  "nest": {
+    "key": "value"
+  }
+},
+"group2": {
+  "abc": 12345,
+  "deep": {
+    "id": 45
+  }
+}
+}
+```
+
+### file02.json:
+```
+{
+"common": {
+  "follow": false,
+  "setting1": "Value 1",
+  "setting3": null,
+  "setting4": "blah blah",
+  "setting5": {
+    "key5": "value5"
+  },
+  "setting6": {
+    "key": "value",
+    "ops": "vops",
+    "doge": {
+      "wow": "so much"
+    }
+  }
+},
+"group1": {
+  "foo": "bar",
+  "baz": "bars",
+  "nest": "str"
+},
+"group3": {
+  "deep": {
+    "id": {
+      "number": 45
+    }
+  },
+  "fee": 100500
+}
+}
+```
+
+### result :
+```
+./bin/gendiff file1.json file2.json
+{
+  common: {
+    + follow: false  
+      setting1: Value 1
+    - setting2: 200  
+    - setting3: true  
+    + setting3: null  
+    + setting4: blah blah
+    + setting5: {
+          key5: value5
+      }
+      setting6: {
+          doge: {
+            - wow: 
+            + wow: so much
+          }
+          key: value
+        + ops: vops
+      }
+  }
+  group1: {
+    - baz: bas
+    + baz: bars
+      foo: bar
+    - nest: {
+          key: value
+      }
+    + nest: str
+  }
+- group2: {
+      abc: 12345
+      deep: {
+          id: 45
+      }
+  }
++ group3: {
+      deep: {
+          id: {
+              number: 45
+          }
+      }
+      fee: 100500
+  }
+}
+```
--- gendif_test.go
+++ gendif_test.go
@@ -14,21 +14,94 @@ func TestGenDiffTest(t *testing.T) {
 		panic(err)
 	}
 
-	//сначала проверяется относительынй путь в текущей папке
-
-	data, err := os.ReadFile(wd + "/" + "fixtures/diff_json.txt")
-	if err != nil {
-		t.Error(err)
+	cases := []struct {
+		name, file1, file2, format, expected string
+		hasError                             bool
+	}{
+		{
+			name:     "json stylish",
+			file1:    "files/file1.json",
+			file2:    "files/file2.json",
+			format:   "stylish",
+			expected: "fixtures/diff_stylish.txt",
+			hasError: false,
+		},
+		{
+			name:     "json plain",
+			file1:    "files/file1.json",
+			file2:    "files/file2.json",
+			format:   "plain",
+			expected: "fixtures/diff_plain.txt",
+			hasError: false,
+		},
+		{
+			name:     "json json",
+			file1:    "files/file1.json",
+			file2:    "files/file2.json",
+			format:   "json",
+			expected: "fixtures/diff_json.txt",
+			hasError: false,
+		},
+		{
+			name:     "yaml stylish",
+			file1:    "files/file1.yaml",
+			file2:    "files/file2.yaml",
+			format:   "stylish",
+			expected: "fixtures/diff_stylish.txt",
+			hasError: false,
+		},
+		{
+			name:     "yaml plain",
+			file1:    "files/file1.yaml",
+			file2:    "files/file2.yaml",
+			format:   "plain",
+			expected: "fixtures/diff_plain.txt",
+			hasError: false,
+		},
+		{
+			name:     "yaml json",
+			file1:    "files/file1.yaml",
+			file2:    "files/file2.yaml",
+			format:   "json",
+			expected: "fixtures/diff_json.txt",
+			hasError: false,
+		},
+		{
+			name:     "yaml stylish",
+			file1:    "files/file1.yml",
+			file2:    "files/file2.yml",
+			format:   "stylish",
+			expected: "fixtures/diff_stylish.txt",
+			hasError: false,
+		},
+		{
+			name:     "yaml plain",
+			file1:    "files/file1.yml",
+			file2:    "files/file2.yml",
+			format:   "plain",
+			expected: "fixtures/diff_plain.txt",
+			hasError: false,
+		},
+		{
+			name:     "yaml json",
+			file1:    "files/file1.yml",
+			file2:    "files/file2.yml",
+			format:   "json",
+			expected: "fixtures/diff_json.txt",
+			hasError: false,
+		},
 	}
-	//mt.Println(want)
-	want := string(data)
-	//fmt.Println(want)
 
-	got, err := GenDiff("files/file1.json", "files/file2.json", "stylish")
-	if err != nil {
-		t.Error(err)
+	for _, r := range cases {
+
+		t.Run(r.name, func(t *testing.T) {
+
+			data, _ := os.ReadFile(wd + "/" + r.expected)
+			want := string(data)
+
+			got, _ := GenDiff(r.file1, r.file2, r.format)
+			require.Equal(t, want, got)
+
+		})
 	}
-	//fmt.Println(got)
-
-	require.Equal(t, want, got)
 }
